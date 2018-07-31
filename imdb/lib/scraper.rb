1744e62e81028cf15a61c5879c66fc13e85eafd2
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-07-31 16:40:38 -0400

    Update scraper.rb

--- imdb/lib/scraper.rb
+++ imdb/lib/scraper.rb
@@ -1,22 +1,22 @@
 require 'nokogiri'
- class SUPERHUMAN::IMDB
-attr_accessor :name, :movie, :url
+ class superman::movie
+attr_accessor :name, :movie, :url, :superhero
 
 def self.hero
   #scrape imdb and wikipedia then return user to based on the data.
-  self.scrape_imdb
+  self.scrape_movie
 end
 
 def self.scrape_imdb
-  imdb = []
+  movie = []
 
-imdb << self .scrape_wikipedia
+movie << self .scrape_wikipedia
 
 # go to imdb find superman 1978 film
 # extract the data
 # instantiate
 
- imdb
+ movie
 end
 
 def self.scrape_imdb
@@ -25,11 +25,32 @@ binding.pry
 end
 
 
-    def url(*path)
-      path.shift if path.first'scores'
-    ('https://en.wikipedia.org/wiki/Superman_(1978_film)')
-    end
+    movie << self.scrape_imdb
+    movie << self.scrape_wikipedia
 
+   movie
+  end
 
+  def self.scrape_imdb
+    doc = Nokogiri::HTML(open('https://www.imdb.com/title/tt0078346/'))
+
+    movie = self.new
+    movie.name = doc.<i>Superman</i>
+    movie.url = "https://www.imdb.com"
+    movie.superhero = true
+
+    movie
+  end
+
+  def self.scrape_wikipedia
+    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Superman_(1978_film)"))
+
+    movie = self.new
+    movie.name = doc.<a href="/year/1978/?ref_=tt_ov_inf">1978</a>
+    movie.plot = doc.<span class="mw-headline" id="Plot">Plot</span>
+    movie.url = "https://www.imdb.com"
+    movie.superhero = true
+
+    movie
   end
 end
