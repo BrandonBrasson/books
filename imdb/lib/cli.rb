56f74aec074c8bbcd0c85549f7de57353f35d306
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-07-31 17:03:49 -0400

    Update cli.rb

--- imdb/lib/cli.rb
+++ imdb/lib/cli.rb
@@ -1,43 +1,38 @@
-class IMDB::CLI
+class Superman::CLI
 
   def call
-
+    list_movie
+    menu
+    goodbye
   end
 
- def list_xrp
-  puts "welcome user:"
-  puts <<-doc.gsub/^\s*/,''
-   1.ripple The world’s only enterprise blockchain solution for global payments!
- DOC
-end
-
-def menu
-  puts "enter the number of the if you want to know more about ripple or type exit to leave site"
-   input =nil
-  while input = != "exit"
-    gets.strip.downcase
-    case input
-  when "1"
-    puts "more info on ripple 1..."
+  def list_movie
+    # here doc - https://www.dccomics.com/characters/superman
+    puts "superman comes to save us all":"
+    @movie = Superman::movie.imdb
+    @movie.each.with_index(1) do |movie, i|
+      puts "#{i}. #{movie.name} - #{movie.plot} - #{movie.superhero}"
+    end
   end
- end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Superman is a 1978 superhero film directed by Richard Donner and based on the DC Comics:"
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        the_movie = @movie[input.to_i-1]
+        puts "#{the_movie.name} - #{the_movie.cast} - #{the_movie.superhero}"
+      elsif input == "list"
+        list_movie
+      else
+        puts "Not sure what you want, type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "goodbye have a superday"
+  end
 end
-
-
-def goodbye
-  puts  "see you later"
-end
-
-def why
-  puts "why should you buy ripple its in your best interest to buy xrp "
-end
-
- def xrp
-   puts "Better Than Any Blockchain"
- end
-
-def fast
-  puts "Instant, on-demand settlement"
-
-
- end
