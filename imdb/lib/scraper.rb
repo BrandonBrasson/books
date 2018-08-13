c349871ecebfe8a443cb25618cd9a5b26cb8cc89
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-08-13 14:32:47 -0400

    Update scraper.rb

--- imdb/lib/scraper.rb
+++ imdb/lib/scraper.rb
@@ -2,29 +2,34 @@
 class Superman::Movie
 attr_accessor :name, :movie, :url, :superhero
 
-def self.imdb
+def self.super
   #scrape imdb and wikipedia then return user to based on the data.
-  self.scrape_movie
+  self.scrape_movies
 end
 
-def self.scrape_imdb
-  movie = []
+def self.scrape_movies
+  movies = []
 
 
-    movie << self.scrape_imdb
-    movie << self.scrape_wikipedia
+    movies << self.scrape_imdb
+    movies << self.scrape_wikipedia
 
- movie
+ movies
   end
 
 
   def self.scrape_imdb
-    doc = Nokogiri::HTML(open('https://www.imdb.com/title/tt0078346/'))
+    doc = Nokogiri::HTML(open('https://www.imdb.com/title/tt0078346/fullcredits?ref_=tt_cl_sm#cast'))
+    list_doc = doc.css("div.findSection")
+    binding.pry
+    list_doc.each.with_index do |element, i|
+      movie = self.new
+      movie.name = list_doc.css(".result_text a")[i].text
+    #binding.pry
+      #movie.plot = doc.<span class="mw-headline" id="Plot">Plot</span>
+      #movie.url = "https://en.wikipedia.org/wiki/Superman_(1978_film)"
+      movie.superhero = true
 
-    movie = self.new
-    #movie.name = doc.<i>Superman</i>
-    movie.url = "https://www.imdb.com"
-    movie.superhero = true
 
     movie
   end
