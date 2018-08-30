c91f45c0e4dde34978ed6d5af11a597782f3b24f
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-08-30 17:55:52 -0400

    Update imdb_spec.rb

--- imdb/spec/imdb_spec.rb
+++ imdb/spec/imdb_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe Imdb do
+RSpec.describe Book do
   it "has a version number" do
-    expect(Imdb::VERSION).not_to be nil
+    expect(Book::VERSION).not_to be nil
   end
 
   it "does something useful" do
