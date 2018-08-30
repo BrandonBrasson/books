b03981a855add23563a7b2e138f105a5de81c089
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-08-30 17:06:35 -0400

    Update scraper.rb

--- imdb/lib/scraper.rb
+++ imdb/lib/scraper.rb
@@ -1,6 +1,14 @@
-require 'nokogiri'
+equire 'nokogiri'
 class Superman::Movie
-attr_accessor :name, :movie, :url, :superhero
+
+  attr_accessor :name, :movie, :url, :superhero
+
+  @@all = [] # This will keep all Movie instances
+
+  # 1. When instances are instantiated, add them to the @@all array/collection (@@all << self)
+  # 2. Create a class reader (e.g. self.all) to allow reading the value of @@all from outside the Movie class
+
+  
    #def self.superhero
    #scrape imdb and wikipedia then return user to based on the data.
  #  self.scrape_imdb
@@ -20,15 +28,14 @@ def self.all
 
     # movie
   end
+
   def self.scrape_imdb
     doc = Nokogiri::HTML(open('https://www.imdb.com/title/tt0078346/fullcredits?ref_=tt_cl_sm#cast'))
     list_doc = doc.css("table.cast_list").css('tr') #get a that will give you the data in the form of Nokogiri objects
-    list_doc.collect.with_index do |element, i|
+    list_doc.collect.with_index(1) do |element, i|
         binding.pry
         movie = self.new
-
-        movie.name = list_doc.css(".result_text a")[i].text
-
+        movie.name = list_doc.css("a")[i].text
       #movie.plot = doc.seach "<tbody><tr><td colspan="4" class="castlist_label"></td></tr>"
       #movie.url = "https://www.imdb.com/title/tt0078346/"
         movie.superhero = true
